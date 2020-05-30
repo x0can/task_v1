89a458e093367a864061ee14ad47b5455784dbf9
alexmwaura <alexmwaura43@gmail.com>
2020-05-30 17:07:15 +0300

    modify

--- functions/authentication/user.js
+++ functions/authentication/user.js
@@ -1,47 +1,40 @@
 const User = require("../models/userModel");
 
-
-
-exports.registerUser =(async (req, res) => {
-
+exports.registerUser = async (req, res) => {
   try {
     const user = new User(req.body);
 
-    await user.save()
+    await user.save();
 
     const token = await user.generateAuthToken();
     return res.status(200).json({
       data: user,
       token,
     });
-
   } catch (error) {
-    // console.error(error);
+    console.error(error.message);
     if (error.name === "MongoError")
       return res
         .status(400)
-        .json({ email: `${req.body.email} already in use`});
+        .json({ email: `${req.body.email} already in use` });
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findByCredentials(email, password);
+    if (!user) {
+      return res.status(400).json({ error: "Account does not exist" });
+    }
+    const token = await user.generateAuthToken();
+    return res.status(200).json({
+      data: user,
+      token,
+    });
+  } catch (error) {
+    console.error(error)
     return res.status(500).json({ general: "Something went wrong" });
   }
-});
-
-exports.loginUser = (async (req,res) => {
-    try{
-        const {email,password} = req.body
-        const user = await  User.findByCredentials(email, password)
-        if(!user){
-            return res.status(400).json({error: "Account does not exist" })
-        }
-        const token = await user.generateAuthToken()
-        return res.status(200).json({
-            data: user,
-            token
-        })
-    }
-    catch (error) {
-        // console.error(error)
-        return res.status(500).json({ general: "Something went wrong" });
-      }
-
-})
-
+};
