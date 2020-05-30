738bba66926a4ff7e460dedaeaba92bccfabab15
alexmwaura <alexmwaura43@gmail.com>
2020-05-30 06:59:45 +0300

    add test cases for authentication

--- test/create_test.js
+++ test/create_test.js
@@ -1,30 +1,45 @@
 const assert = require("assert")
-const User = require("../functions/models/userModel")
+const chai = require('chai')
+
+const {loginUser,registerUser} =  require("../functions/authentication/user")
+
+const expect = chai.expect;
+const chaiHttp = require("chai-http")
+chai.use(chaiHttp)
+const app = require("../index")
 
 const userData = {
     username: "test user",
     email: "testuser@mail.com",
-    password: "1234test"
+    password: "1234test",
+    role: "merchant"
 }
 
 describe('Creating user', () => {
     it("creates new user", (done) => {
-        const user = new User(userData)
-        user.save()
-        .then(() => {
-            assert(!user.isNew);
-            done()
-
-        })
+       chai.request(app).post("/api/v1/signup", registerUser).send(userData).then(res => {
+           expect(res.body.token)
+           done()
+       }).catch(err => {
+           done(err)
+       })
+        
     })
+    
 })
 
 describe('Login in User', () => {
-    it('logins in a user', (done) => {
+
+    it("logins in a user", (done) => {
         const {email,password} = userData
-        console.log(email)
-        const registeredUser = User.findByCredentials(email,password)
-        assert(registeredUser)
-        done()
+        const loginCredentials = {email: email, password: password}
+        chai.request(app).post("/api/v1/login", loginUser).send(loginCredentials).then(res => {
+            // console.log(res)
+            expect(res.body.token)
+            done()
+        }).catch(err => {
+            done(err)
+        })
     })
+
 })
