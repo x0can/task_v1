9a21d64cd57a65d164b8a01196f891d38d34e519
alexmwaura <alexmwaura43@gmail.com>
2020-05-30 19:12:53 +0300

    refactor

--- functions/models/itemsModel.js
+++ functions/models/itemsModel.js
@@ -2,37 +2,27 @@ const mongoose = require("mongoose");
 const User = require("./userModel");
 
 const itemsSchema = mongoose.Schema({
-  name: { type: String, required: true, trim: true },
-  price: { type: Number, required: true, trim: true },
-  userId: { type: String, required: true },
-  amount: { type: Number },
-});
+  name: { 
+    type: String,
+     required: true,
+      trim: true 
+    },
+  price: { 
+    type: Number,
+     required: true,
+      trim: true 
+    },
+  userId: { 
+    type: String, 
+    required: true 
+  },
+  amount: { 
+    type: Number
+   },
+})
 
-// itemsSchema.pre("save", async (next) => {
-//     const itemData = this
-//     const userData = User.findById(user.userId).then(user => {
-//         itemData.user = user
-//     })
-//     next()
 
-// })
 
-itemsSchema.methods.addAmount = async (amount) => {
-  const item = this;
-  item.amount = amount;
-  item.update(amount);
-  return amount;
-};
+const Items = mongoose.model("Items", itemsSchema);
 
-itemsSchema.methods.getUserDetails = async (userId) => {
-  const item = this;
-  const userData = User.findById(userId)
-    .then((user) => {
-      return user;
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-};
-
-module.exports = mongoose.model("Items", itemsSchema);
+module.exports = Items
