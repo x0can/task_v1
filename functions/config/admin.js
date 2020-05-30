c6f4a19ce0b6b41574d1257c4a16b9def1c90aa1
alexmwaura <alexmwaura43@gmail.com>
2020-05-30 06:56:37 +0300

    initialize db

---
+++ functions/config/admin.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+const url = "mongodb://localhost:27017"
+
+mongoose.Promise = global.Promise
+
+mongoose.connect(url, {
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useNewUrlParser: true
+})
+
+exports.connectDB =  () => {
+    mongoose.connection
+    .on('error', console.error.bind(console, 'connection error'))
+    .once('open', ()=> {
+        console.log('Connection Successful!')
+    });
+
+    beforeEach((done) => {
+        mongoose.connection.collections.users.drop(() => {
+            done();
+        })
+    })   
+
+}
+
