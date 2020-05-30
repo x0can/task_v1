58db4097918a72c6414f47c102cd1f45635f7d84
alexmwaura <alexmwaura43@gmail.com>
2020-05-30 17:08:42 +0300

    add schema

--- data/data.js
+++
@@ -1,9 +0,0 @@
-exports.testData = (req,res) => {
-
-
-
-
-    
-    console.log(req.user)
-    return res.status(201).json({success: true})
-}
--- functions/models/itemsModel.js
+++ functions/models/itemsModel.js
@@ -1,6 +1,38 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const User = require("./userModel");
 
 const itemsSchema = mongoose.Schema({
-    name: {type: String,required: true,trim: true },
-    price: {type: Number, required: true, trim: true}
-})
+  name: { type: String, required: true, trim: true },
+  price: { type: Number, required: true, trim: true },
+  userId: { type: String, required: true },
+  amount: { type: Number },
+});
+
+// itemsSchema.pre("save", async (next) => {
+//     const itemData = this
+//     const userData = User.findById(user.userId).then(user => {
+//         itemData.user = user
+//     })
+//     next()
+
+// })
+
+itemsSchema.methods.addAmount = async (amount) => {
+  const item = this;
+  item.amount = amount;
+  item.update(amount);
+  return amount;
+};
+
+itemsSchema.methods.getUserDetails = async (userId) => {
+  const item = this;
+  const userData = User.findById(userId)
+    .then((user) => {
+      return user;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+module.exports = mongoose.model("Items", itemsSchema);
