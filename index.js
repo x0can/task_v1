6df86c9c514bc6a914e1455a061867f2073de2d3
alexmwaura <alexmwaura43@gmail.com>
2020-05-30 06:56:13 +0300

    export app

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {connectDB} = require("./config/admin")
+const {connectDB} = require("./functions/config/admin")
 const path = require("path")
 const dotenv = require("dotenv")
 dotenv.config({path: './.env'})
@@ -12,22 +12,23 @@ const app =express()
 app.use(express.json())
 
 // custom functions
-const {registerUser,loginUser} = require("./authentication/user")
-const AuthMidd = require("./middleware/allAuth")
+const {registerUser,loginUser} = require("./functions/authentication/user")
+const AuthMidd = require("./functions/middleware/allAuth")
 
 
 // test function for the middleware
-const {testData} = require("./data/data")
 
 // routes
 app.post("/api/v1/signup", registerUser)
 app.post("/api/v1/login", loginUser)
 
 // test route for the middleware
-app.post("/api/v1/test", AuthMidd, testData)
+app.post("/api/v1/test", AuthMidd)
 
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT,()=> console.log(`\n running \n mode: ${process.env.NODE_ENV} \n on: http://localhost:${PORT}`))
+
+module.exports =app
