f395225985fc9e60b8a807ff6cdb130382a05816
alexmwaura <alexmwaura43@gmail.com>
2020-06-01 15:10:06 +0300

    modify

--- functions/models/orderModel.js
+++ functions/models/orderModel.js
@@ -17,38 +17,5 @@ const OrderSchema = mongoose.Schema({
    
 })
 
-// OrderSchema.statics.addOrder = async function(itemId) {
-//     const order = this  
-//     const itemData =  await Items.findById(itemId).then(item=> {
-//         if(!item){
-//             throw new Error({error: "Item not found"})
-//         }
-//         return item    
-//     })
-
-//     order.find({}).populate(`${itemData}`).exec((err, items) => {
-//         if(err){
-//             console.log(err)
-//             throw Error({error: err})
-//         }
-
-       
-
-//         items.forEach(newItem=> {
-//             newItem.itemData.forEach(orderItem => {
-//                 if(new String(orderItem._id).valueOf() !== new String(itemId).valueOf()){
-                    
-//                 }
-//             })
-//         })
-//     })
-
- 
-    // order.itemData.push(item)
-
-    // return order    
-    
-// }
-
 const Order = mongoose.model("Order", OrderSchema)
 module.exports = Order
